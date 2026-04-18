b4e723a3af58def65ffcd1872fdf01fa01acf9ac
Colinz <cuongbzo123@gmail.com>
2026-04-19 01:19:43 +0700

    Fix: Cập nhật lại tên  database

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ global $conn;
 function connectDB(){
     global $conn;
     if(!$conn){
-        $conn = mysqli_connect("34.126.147.241","md162","Logistic@2026","logistics_db") 
+        $conn = mysqli_connect("34.126.147.241","md162","Logistic@2026","logistic") 
                 or die("Can not connect to database".mysqli_connect_error());
         mysqli_set_charset($conn, "utf8mb4");
 }
